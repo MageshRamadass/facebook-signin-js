db0754767c52044509afa180118278fb3d81448b
MageshRamadass <magesh.r@zebuetrade.com>
2022-12-14 16:47:56 +0530

    first push

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,61 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="signup-buttons">
+    <!-- <div id="fb-root"></div> -->
+    <v-btn>
+      <a href="#" class="facebook-signup" @click.prevent="loginWithFacebook">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="#3578E5"
+        >
+          <path
+            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
+          />
+        </svg>
+        Facebook
+      </a></v-btn
+    >
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { initFbsdk } from '@/main.js'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'login_signup_social',
+  mounted () {
+    initFbsdk()
+  },
+  methods: {
+    loginWithFacebook () {
+      window.FB.login(response => {
+        console.log('fb response', response)
+        this.accessToken = response
+        this.getUserDetails()
+      }, this.params)
+    },
+    getUserDetails() {
+      
+            var config = {
+              method: 'post',
+              url: 'https://graph.facebook.com/me?fields=email,name,gender,about&access_token='+this.accessToken.authResponse.accessToken,
+              headers: { }
+            };
+
+            axios(config)
+            .then(function (response) {
+              console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,44 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+// import { initializeApp } from "firebase/app";
+// import firebase from 'firebase/app';
+// import firebase from 'firebase/compat/app';
+/* eslint-disable */
+export const initFbsdk = () => {
+  return new Promise(resolve => {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId : '479404944279138',
+        cookie : true,
+        xfbml : true, // parse social plugins on this page
+        version : 'v2.8' // use graph api version 2.8
+      });
+    };
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  })
+}
+// console.log('f',firebase)
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC9WZkncGsa52xKwhZbfLQS3kKGiQ4HOWs",
+//   authDomain: "signin-226ac.firebaseapp.com",
+//   projectId: "signin-226ac",
+//   storageBucket: "signin-226ac.appspot.com",
+//   messagingSenderId: "55922781779",
+//   appId: "1:55922781779:web:bbfb7a4c07a15fa5e3a0a6"
+// };
+// firebase.initializeApp(firebaseConfig);
+// initializeApp(firebaseConfig);
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
